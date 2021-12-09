87531199f0450f935c675bc3eaa4f21fe75ba838
Keita Nakamura <keita.nakamura.1109@gmail.com>
2021-12-09 20:56:39 +0900

    Fix extract_forces for forces below the tip (#24)

--- src/PilePenetration.jl
+++ src/PilePenetration.jl
@@ -367,14 +367,19 @@ function extract_contact_forces(fcᵢ, grid, pile)
     inside = Dict{Float64, Float64}()
     outside = Dict{Float64, Float64}()
     for I in eachindex(fcᵢ) # walk from lower height
-        y = grid[I][2] - tip_height(pile)
         fcy = -2π * fcᵢ[I][2]
         iszero(fcy) && continue
-        centerline1 = Line(((getline(pile, 1) + reverse(getline(pile, 5))) / 2)...)
-        centerline2 = Line(((getline(pile, 2) + reverse(getline(pile, 4))) / 2)...)
-        isinside = GeometricObjects.ray_casting_to_right(centerline1, grid[I]) ||
-        GeometricObjects.ray_casting_to_right(centerline2, grid[I])
+        if grid[I][2] > pile[3][2] # straight and tapered part
+            centerline1 = Line(((getline(pile, 1) + reverse(getline(pile, 5))) / 2)...)
+            centerline2 = Line(((getline(pile, 2) + reverse(getline(pile, 4))) / 2)...)
+            isinside = GeometricObjects.ray_casting_to_right(centerline1, grid[I]) ||
+                       GeometricObjects.ray_casting_to_right(centerline2, grid[I])
+        else # below the tip
+            center = (pile[3][1] + pile[4][1]) / 2
+            isinside = grid[I][1] < center
+        end
         output = isinside ? inside : outside
+        y = grid[I][2] - tip_height(pile)
         output[y] = get(output, y, 0.0) + fcy
     end
     function dict2array(dict)
